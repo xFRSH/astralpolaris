fa20b6b5fe66384294d7e25ee341ddd08b27f5d8
xFRSH <101560537+xFRSH@users.noreply.github.com>
2022-06-01 13:31:04 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-    $db_name = "percobaan";
-    $username = "root";
-    $password = "";
-    $servename = "localhost";
+    $db_name = "epiz_31823812_percobaan";
+    $username = "epiz_31823812";
+    $password = "8x2KundcrFOlKH";
+    $servename = "sql302.epizy.com";
     $conn = mysqli_connect($servename ,$username ,$password ,$db_name );
 
 ?>
